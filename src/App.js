98bf5d3a9ffae4bd02302e4a6c43efa82cc15a49
chenaoh <rabanalesf22@gmail.com>
2023-08-30 22:32:07 -0700

    SE AGREGA EL PROYECTO

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Person(){
+  return (
+    <div className="person">
+      <h3>Nombre: Pedro</h3>
+      <p>Edad: 25</p>
+    </div>
+  )
+}
+function Lista(){
+  return (
+    <div className="lista">
+      <h3>Numeros</h3>
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hola mundo</h1>
+      <Person />
+      <Lista />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
